71e212bb04511b7d64403667b66c1bec05903b5b
Juanfe <pipe081970@hotmail.com>
2021-02-26 12:39:23 -0500

    prueba

---
+++ precache-manifest.9ef502b3f8c4ffcaa3dc60964643ae1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e342b45217b07d864d77a2c2f7fd0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42837c1466a7a8df673c",
+    "url": "/static/css/main.617f302c.chunk.css"
+  },
+  {
+    "revision": "b64d85d508b8b06a50af",
+    "url": "/static/js/2.0367674d.chunk.js"
+  },
+  {
+    "revision": "dad1e10c751afc1e6fffeb8be63f9aab",
+    "url": "/static/js/2.0367674d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42837c1466a7a8df673c",
+    "url": "/static/js/main.677ec84b.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
